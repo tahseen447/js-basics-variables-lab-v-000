87a34f76d1879e6f180d3261887aee8e6db2fd1d
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-11-02 07:16:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-// Code your solution in this file!
-describe('index.js', function () {
-  // there's stuff in here, too
-  it('is set as Scuber', function () {
-  expect(companyName).to.equal('Scuber');
-});
-});
 
 const companyName = 'Scuber';
 
